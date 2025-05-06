fb11c8e3ee71e87052c4e5c5681737178c1d6199
Ulrich Müller <uvm@talionis.net>
2025-05-06 23:27:23 +0200

    Run multiple exiftool commands to handle fallbacks properly

--- takeout-fix.sh
+++ takeout-fix.sh
@@ -10,11 +10,39 @@ if [ -z "$DIR" ]; then
   exit 1
 fi
 
-echo "Deleting json metadata..."
-find "$DIR" -type f -name '*.json' -delete
+echo "Rename metadata json to be consistent..."
+find "$DIR" -type f -name '*.json' -print0 \
+    | perl -0 -lne '@parts=split /\./; $base=join(".", @parts[0..$#parts-2]); rename $_, "$base.meta"'
 
 echo "Updating image and video modify dates..."
-exiftool -r '-FileModifyDate<${filename; s/.*?(20\d{2})(\d{2})(\d{2}).*/$1-$2-$3 00:00:0/}' '-FileModifyDate<CreateDate' '-FileModifyDate<DateTimeOriginal' "$DIR"
+# From Google metadata
+exiftool -r -m \
+    -d '%s' \
+    -tagsFromFile '%d%f.%e.meta' \
+    '-FileModifyDate<PhotoTakenTimeTimestamp' \
+    -overwrite_original \
+    "$DIR"
+# Videos
+exiftool -r -m \
+    -if '$MediaModifyDate' \
+    '-FileModifyDate<MediaCreateDate' \
+    -overwrite_original \
+    "$DIR"
+# Overwrite with DateTimeOriginal if present
+exiftool -r -m \
+    -if '!$MediaModifyDate' \
+    '-FileModifyDate<DateTimeOriginal' \
+    -overwrite_original \
+    "$DIR"
 
 echo "Updating Pixel motion photo modify dates..."
-exiftool -r -ext MP '-FileModifyDate<${filename; s/.*?(20\d{2})(\d{2})(\d{2}).*/$1-$2-$3 00:00:0/}' '-FileModifyDate<CreateDate' '-FileModifyDate<DateTimeOriginal' "$DIR"
+exiftool -r -m \
+    -ext MP \
+    -d '%s' \
+    -tagsFromFile '%d%f.%e.jpg.meta' \
+    '-FileModifyDate<PhotoTakenTimeTimestamp' \
+    -overwrite_original \
+    "$DIR"
+
+echo "Deleting json metadata..."
+find "$DIR" -type f -name '*.meta' -delete
